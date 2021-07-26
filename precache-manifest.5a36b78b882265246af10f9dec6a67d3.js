40dc88a0cc4399a49fd6530cdad9ce41db662cea
pratik8696 <mepratikanand@gmail.com>
2021-07-26 16:10:20 +0530

    Updates

---
+++ precache-manifest.5a36b78b882265246af10f9dec6a67d3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce212fbc5792e989f063d7986b074839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e27eb4247f1e2aedae0",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "488b4df15096aec9f90a",
+    "url": "/static/js/2.5f8788fa.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.5f8788fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e27eb4247f1e2aedae0",
+    "url": "/static/js/main.ea4d8a89.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "47831f00ecc74da30c00ee4f78cffabc",
+    "url": "/static/media/IEEE.47831f00.png"
+  },
+  {
+    "revision": "3d9c987e29dd1cf73f686f7dbd6d49f1",
+    "url": "/static/media/hackslash.3d9c987e.png"
+  },
+  {
+    "revision": "4e6fa7b3eaa2479d0c76b39b8cf29292",
+    "url": "/static/media/pratikanand.4e6fa7b3.png"
+  },
+  {
+    "revision": "e24a20a5cce454f42897d2c230167367",
+    "url": "/static/media/resume.e24a20a5.pdf"
+  },
+  {
+    "revision": "5c630d8be353f0b236a4d9a73d084605",
+    "url": "/static/media/robotics.5c630d8b.jpg"
+  }
+]);
